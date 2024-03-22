8f2fd06f63572ac856d3051cb17febea5ff77bf5
Shah Darshan <darshanshah8922@gmail.com>
2024-03-22 11:06:21 +0530

    try1

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,7 +4,7 @@ from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
 class CustomUserAdmin(UserAdmin):
-    print("testinghnh")
+    print("testinghnhj")
     list_display = ('email', 'first_name', 'last_name', 'username', 'role', 'is_active')
     ordering = ('-date_joined',)
     filter_horizontal = ()
