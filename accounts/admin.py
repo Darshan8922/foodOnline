d3becd26106ad1ad1864ce8d683b9666beb1ae26
Shah Darshan <darshanshah8922@gmail.com>
2024-03-22 10:56:24 +0530

    added logs

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,6 +4,7 @@ from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
 class CustomUserAdmin(UserAdmin):
+    print("testing")
     list_display = ('email', 'first_name', 'last_name', 'username', 'role', 'is_active')
     ordering = ('-date_joined',)
     filter_horizontal = ()
